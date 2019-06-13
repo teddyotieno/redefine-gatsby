f8cf7cf55475bdff350b00f45da3be381038f61c
Teddy Otieno <asolatotieno@gmail.com>
2019-06-14 02:38:17 +0300

    Header and header-wrapper style components

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,37 @@
-import { Link } from "gatsby"
+import styled from "@emotion/styled"
 import React from "react"
+import { Link } from "gatsby"
 
-const Header = () => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          Redefine Systems
-        </Link>
-      </h1>
-    </div>
-  </header>
+const HeaderWrapper = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 72px;
+  background: #fff;
+  box-shadow: 0 1px 2px 0 rgba(36, 50, 66, 0.15);
+  z-index: 1;
+`
+
+const Header = styled.header`
+  width: 75%;
+  margin: 0 auto;
+  height: inherit;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Nav = styled.nav`
+  display: flex;
+`
+
+export default () => (
+  <HeaderWrapper>
+    <Header>
+      &nbsp;
+      <Nav>
+        <Link to="/page-2">Log In</Link>
+        <Link to="/signup">Get Started</Link>
+      </Nav>
+    </Header>
+  </HeaderWrapper>
 )
-
-export default Header
